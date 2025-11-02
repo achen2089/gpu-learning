6f974df95d9101b8e3ae1a786edf218aea23ac3d
achen2089 <achen2089work@gmail.com>
2025-11-02 10:43:15 -0800

    count all devices

--- scratch.py
+++ scratch.py
@@ -9,4 +9,6 @@ print(f"Current device: {torch.cuda.current_device()}")  # Usually 0
 # Get device name
 print(f"Device 0: {torch.cuda.get_device_name(0)}")
 if torch.cuda.device_count() > 1:
-    print(f"Device 1: {torch.cuda.get_device_name(1)}")
+    for i in range(1, torch.cuda.device_count()):
+        print(f"Device {i}: {torch.cuda.get_device_name(i)}")
+        
